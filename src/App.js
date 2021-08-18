ba868259d84aac8f4ed4ba286e585ab4ac35233e
Renier Arias <renier.arias@gmail.com>
2021-08-17 23:01:16 -0400

    video 35

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ class App extends Component {
     }
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
@@ -26,9 +30,10 @@ class App extends Component {
     )
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder='search monsters'
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
